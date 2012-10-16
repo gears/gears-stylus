48701a9f582d65f8086b2bdefe02315d6aca1e77
Mike Yumatov <mike@yumatov.org>
2012-10-16 10:01:38 +0400

    Drop Python 2.5 support, add support for Python 3.2

--- setup.py
+++ setup.py
@@ -23,8 +23,8 @@ setup(
         'License :: OSI Approved :: ISC License (ISCL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
     ],
 )
